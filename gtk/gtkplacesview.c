568f8acbff154f8a758f568a0e616578755aa272
William Hua <william.hua@canonical.com>
2015-09-25 09:47:42 -0400

    gtkplacesview: use gtk_menu_popup_with_parameters ()

--- gtk/gtkplacesview.c
+++ gtk/gtkplacesview.c
@@ -1713,13 +1713,13 @@ popup_menu (GtkPlacesViewRow *row,
       button = 0;
     }
 
-  gtk_menu_popup (GTK_MENU (priv->popup_menu),
-                  NULL,
-                  NULL,
-                  NULL,
-                  NULL,
-                  button,
-                  event ? event->time : gtk_get_current_event_time ());
+  gtk_menu_popup_with_parameters (GTK_MENU (priv->popup_menu),
+                                  NULL,
+                                  NULL,
+                                  NULL,
+                                  button,
+                                  event ? event->time : gtk_get_current_event_time (),
+                                  NULL);
 }
 
 static gboolean
